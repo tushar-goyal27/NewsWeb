45de1301e4983546fd42f95e8b4872aa898cfe9f
Tushar Goyal <tushar.lifetg@gmail.com>
2021-12-22 12:35:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ArticleList from './components/ArticleList';
+import InputKey from './components/InputKey';
+
 
 function App() {
+  const [artList, setArtList]= useState([])
+  
+  const getdata = (e, keyword, country) => {
+    // call back function
+    e.preventDefault();
+    fetch("http://localhost:5000/news", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({'keyword': keyword, 'country': country})
+    }).
+    then(res => res.json()).
+    then(data => {
+      setArtList(data.articles)
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="mt-3">
+        <InputKey getnewsdata={ (e, keyword, country) => getdata(e, keyword, country) }/>
+      </div>
+      <div>
+        {artList && <ArticleList listOfArcticles={artList} />}
+      </div>
     </div>
   );
-}
+};
 
+// const waterEffect = {
+//   "background": "rgb(84, 164, 222)", 
+//   "height": "10%"
+// }
 export default App;
